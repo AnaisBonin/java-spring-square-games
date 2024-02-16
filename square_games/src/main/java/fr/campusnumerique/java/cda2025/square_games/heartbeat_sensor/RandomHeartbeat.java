ced1e2f0ec249c81b01e00fe19a807e3ea911d01
anais <anais.bonin@le-campus-numerique.fr>
2024-02-16 12:36:09 +0100

    Implement random in get method for a number between 40 and 231

--- square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/heartbeat_sensor/RandomHeartbeat.java
+++ square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/heartbeat_sensor/RandomHeartbeat.java
@@ -2,10 +2,16 @@ package fr.campusnumerique.java.cda2025.square_games.heartbeat_sensor;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 public class RandomHeartbeat implements HeartbeatSensor{
     @Override
     public int get() {
-        return 0;
+        Random random = new Random();
+        int min = 40;
+        int max = 231;
+
+        return random.nextInt(max-min) + min;
     }
 }
