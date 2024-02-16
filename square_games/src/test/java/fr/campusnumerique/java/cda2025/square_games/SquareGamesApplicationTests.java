de8ca7ff988a240fc8c2b807bcd4fbbe41c307c0
anais <anais.bonin@le-campus-numerique.fr>
2024-02-16 11:38:43 +0100

    Boilerplate project

---
+++ square_games/src/test/java/fr/campusnumerique/java/cda2025/square_games/SquareGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.campusnumerique.java.cda2025.square_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
