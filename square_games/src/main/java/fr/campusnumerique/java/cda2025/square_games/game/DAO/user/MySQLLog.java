af45c15ee290bea6c773471d0b5a11186c555ac9
anais <anais.bonin@le-campus-numerique.fr>
2024-02-23 12:39:09 +0100

    Add MySQL log properties and log file

---
+++ square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/game/DAO/user/MySQLLog.java
@@ -0,0 +1,9 @@
+package fr.campusnumerique.java.cda2025.square_games.game.DAO.user;
+
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.Logger;
+
+public class MySQLLog {
+    private static final Logger log = (Logger) LoggerFactory.getLogger(MySQLLog.class);
+}
