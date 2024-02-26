be641fe292e153a0c8529052d9e439345f4bec4d
AnaisBonin <anais.bonin@le-campus-numerique.fr>
2024-02-26 11:05:17 +0100

    Remove methods visibility in UserDAO interface

--- square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/game/DAO/user/UserDAO.java
+++ square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/game/DAO/user/UserDAO.java
@@ -1,16 +1,19 @@
 package fr.campusnumerique.java.cda2025.square_games.game.DAO.user;
 
 import fr.campusnumerique.java.cda2025.square_games.game.controller.DO.User;
-import fr.campusnumerique.java.cda2025.square_games.game.controller.DTO.UserDTO;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface UserDAO {
-    public List<User> getAllUsers();
-    public User getUserById(UUID id);
-    public User addUser(User user);
-    public User updateUser(UUID id, User user);
-    public void deleteUser(UUID id);
+    List<User> getAllUsers();
+
+    User getUserById(UUID id);
+
+    User addUser(User user);
+
+    User updateUser(UUID id, User user);
+
+    void deleteUser(UUID id);
 }
 
