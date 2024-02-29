c214c72a29576de28ee7dfa2bdc3cef80f332550
AnaisBonin <anais.bonin@le-campus-numerique.fr>
2024-02-29 14:26:30 +0100

    Create User ENTITY

---
+++ square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/game/entities/User.java
@@ -0,0 +1,46 @@
+package fr.campusnumerique.java.cda2025.square_games.game.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String pseudo;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
