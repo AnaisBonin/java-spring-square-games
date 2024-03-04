e9c266a053806bcb455500be339a1ac38224238f
AnaisBonin <anais.bonin@le-campus-numerique.fr>
2024-03-04 11:02:47 +0100

    Add UserDEtailsService class

---
+++ square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/security/MyUserDetailsService.java
@@ -0,0 +1,12 @@
+package fr.campusnumerique.java.cda2025.square_games.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
