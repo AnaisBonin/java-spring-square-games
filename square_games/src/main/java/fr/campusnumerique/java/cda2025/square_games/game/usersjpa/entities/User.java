890d9eaa501504eb5dd45a607dead929dffd8d18
AnaisBonin <anais.bonin@le-campus-numerique.fr>
2024-03-04 11:29:14 +0100

    Add UserDetails to User Entity

--- square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/game/usersjpa/entities/User.java
+++ square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/game/usersjpa/entities/User.java
@@ -1,10 +1,14 @@
 package fr.campusnumerique.java.cda2025.square_games.game.usersjpa.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     private Integer id;
@@ -55,5 +59,40 @@ public class User {
 
     public UserJpaDTO toJpaDTO() {
         return new UserJpaDTO(this.firstName, this.lastName, this.pseudo);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
