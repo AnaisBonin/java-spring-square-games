b2da89441bc3310c0da9157ccc80da7d994455f7
AnaisBonin <anais.bonin@le-campus-numerique.fr>
2024-03-04 10:20:28 +0100

    Add printStackTrace in SpringApplication to print errors

--- square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/SquareGamesApplication.java
+++ square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/SquareGamesApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SquareGamesApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(SquareGamesApplication.class, args);
+        try {
+            SpringApplication.run(SquareGamesApplication.class, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
