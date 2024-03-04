6e00c354a34c12bb725233821ca9ca18121db072
AnaisBonin <anais.bonin@le-campus-numerique.fr>
2024-03-04 16:02:11 +0100

    UserDetailsService load userInformations

--- square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/security/MyUserDetailsService.java
+++ square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/security/MyUserDetailsService.java
@@ -1,12 +1,19 @@
 package fr.campusnumerique.java.cda2025.square_games.security;
 
+import fr.campusnumerique.java.cda2025.square_games.game.usersjpa.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return userRepository.findByUsername(username);
     }
 }
