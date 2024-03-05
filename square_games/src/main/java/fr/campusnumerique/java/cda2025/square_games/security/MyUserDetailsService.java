f3749fc03bfcaf43d3e0d9258f66b775c1770bb4
AnaisBonin <anais.bonin@le-campus-numerique.fr>
2024-03-05 11:27:06 +0100

    Edit UserDetails to return a user Builder

--- square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/security/MyUserDetailsService.java
+++ square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/security/MyUserDetailsService.java
@@ -2,6 +2,7 @@ package fr.campusnumerique.java.cda2025.square_games.security;
 
 import fr.campusnumerique.java.cda2025.square_games.game.usersjpa.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,8 +13,15 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username);
+        var user = userRepository.findByUsername(username);
+        return User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .authorities(user.getAuthorities())
+                .build();
     }
 }
