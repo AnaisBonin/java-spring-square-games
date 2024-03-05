ba29d4f5ac476c15b46b86d8c2824324c62bacfc
AnaisBonin <anais.bonin@le-campus-numerique.fr>
2024-03-05 11:26:49 +0100

    Authentication handlers

---
+++ square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/security/AuthenticationEvents.java
@@ -0,0 +1,21 @@
+package fr.campusnumerique.java.cda2025.square_games.security;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationEvents {
+    @EventListener
+    public void onSuccess(AuthenticationSuccessEvent success) {
+        System.out.println(success);
+        System.out.println("Authentication successful");
+    }
+
+    @EventListener
+    public void onFailure(AbstractAuthenticationFailureEvent failures) {
+        System.out.println(failures);
+        System.out.println("Authentication failed");
+    }
+}
