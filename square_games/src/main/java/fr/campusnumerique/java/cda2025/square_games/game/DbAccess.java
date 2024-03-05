945cc81bbcef1d1361a062399ff388c9db0af023
AnaisBonin <anais.bonin@le-campus-numerique.fr>
2024-03-05 12:12:58 +0100

    Make access to DB safe

--- square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/game/DbAccess.java
+++ square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/game/DbAccess.java
@@ -2,6 +2,7 @@ package fr.campusnumerique.java.cda2025.square_games.game;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,6 +11,15 @@ import java.sql.SQLException;
 public class DbAccess {
     private static DbAccess instance;
     private Connection connection;
+
+    @Value("${spring.datasource.url}")
+    private String databaseUrl;
+
+    @Value("${spring.datasource.username}")
+    private String databaseUsername;
+
+    @Value("${spring.datasource.password}")
+    private String databasePassword;
 
     public static DbAccess getInstance() {
         if (instance == null) instance = new DbAccess();
@@ -24,7 +34,7 @@ public class DbAccess {
         if (connection == null) {
             try {
                 connection = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:6603/boardgames", "root", "helloworld");
+                        databaseUrl, databaseUsername, databasePassword);
                 LOGGER.info("New connection to boardgames database");
             } catch (SQLException e) {
                 LOGGER.error("Could not connect to database");
