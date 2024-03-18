eab6592f90b338bb492bdedce702d7ff799a971f
AnaisBonin <anais.bonin@le-campus-numerique.fr>
2024-03-18 09:25:49 +0100

    add userLoginDTA and userJPA Controller

--- square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/game/usersjpa/controllers/UserJpaController.java
+++ square_games/src/main/java/fr/campusnumerique/java/cda2025/square_games/game/usersjpa/controllers/UserJpaController.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("user-jpa")
+@RequestMapping("api")
 public class UserJpaController {
     @Autowired
     private UserRepository userRepository;
